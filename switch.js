b7f461efaf486ae714347ac38daed41bba882802
[Mukesh Kumar Utmani] <[kumarmukesh789123@gmail.com]>
2023-05-23 14:12:48 +0500

    first commit

---
+++ switch.js
@@ -0,0 +1,11 @@
+var day='sunday';
+switch(day){
+    case'monday': console.log("day was boorng");
+    break;
+    case'tuesday': console.log('Fnatastic day');
+    break;
+    case'friday': console.log('Prayer Day');
+    break;
+    default: console.log('all are holidays');
+}
+
